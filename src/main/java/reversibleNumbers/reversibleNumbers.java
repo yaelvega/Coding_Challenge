df42bff000731dc65352b5d1463bf8b8ced211b0
Yael Vega <yaelvega94@outlook.com>
2018-07-12 14:28:02 -0500

    Implemented reversible numbers

--- src/main/java/reversibleNumbers/reversibleNumbers.java
+++ src/main/java/reversibleNumbers/reversibleNumbers.java
@@ -6,7 +6,36 @@ public class reversibleNumbers {
 
     // Bonus tip, maybe int it's not the correct type for calculating the reverse of a 1x10e9 number
     static int reversible(int N) {
-        throw new UnsupportedOperationException("Waiting to be implemented");
+        int count = 0;
+        long reverseNumber, tmp;
+        boolean isValid;
+        for(int value = 1; value <= N; value++) {
+            //No leading zeroes
+            if((value % 10) != 0) {
+                //reverse calculation
+                reverseNumber = 0;
+                tmp = value;
+                while(tmp != 0) {
+                    reverseNumber *= 10;
+                    reverseNumber += tmp % 10;
+                    tmp /= 10;
+                }
+
+                //sum verification
+                isValid = true;
+                tmp = value + reverseNumber;
+                while(tmp != 0) {
+                    if((tmp % 2) == 0) {
+                        isValid = false;
+                        break;
+                    }
+                    tmp /= 10;
+                }
+                if(isValid)
+                    count++;
+            }
+        }
+        return count;
     }
 
 
